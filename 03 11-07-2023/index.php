2c394f6f9d7e5bf5ecccab2a2ffb916699ca02f4
Md Jahid Hasan <80060849+hmjahid@users.noreply.github.com>
2023-07-11 23:58:23 +0600

    Add new practise

---
+++ 03 11-07-2023/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>PHP Document</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+
+       <h2>I am a Front-End, WordPress and WooCommerce Developer</h2>
+
+       <?php 
+        
+        // Single Line Comment
+        # This is also single line comment
+
+        echo "Welcome to PHP. \n";
+        print "Hello World. \n";
+
+        ECHO "Be active. \n";
+        PRINT "Work hard.";
+
+        $age = 27;
+        $food = "hotchpoch";
+        $Food = "chanachur";
+
+        echo "I am Md Jahid Hasan. I am " . $age . " years of old. I love to eat " . $food . ". I also love to eat " . $Food . ".";
+
+        $name = "Jahid";
+        $Name = " Jahid";
+        $_name = "Jahid";
+        $NAME = "Jahid";
+
+        echo "My name is " . $name . $Name . $_name . $NAME;
+
+        /*
+        Multiple
+        Line
+        Comment
+        */
+
+        ?>
+    
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,10 @@
+1. Monday : (8 AM -1 PM)(2PM to 5PM)
+2. Monday : (8 AM -1 PM)(2PM to 5PM)
+3. Monday : (8 AM -1 PM)(2PM to 5PM)
+4. Monday : (8 AM -1 PM)(2PM to 5PM)
+5. Monday : (8 AM -1 PM)(2PM to 5PM)
+6. Monday : (8 AM -1 PM)(2PM to 5PM)
+7. Monday : (8 AM -1 PM)(2PM to 5PM)
+
+
+
