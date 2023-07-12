91011e68b1dfeb12730dcd3254239166159cee53
Md Jahid Hasan <80060849+hmjahid@users.noreply.github.com>
2023-07-12 22:07:50 +0600

    Add new practise

---
+++ 04 12-07-2023/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>PHP Document</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+
+       <?php 
+        
+        $x = 10;
+        $y = 20;
+
+        print $x + $y . "<br>";
+        echo $x + $y . "<br>";
+        print ($x + $y) . "<br>";
+        echo ($x + $y);
+
+        ?>
+    
+    </body>
+</html>
